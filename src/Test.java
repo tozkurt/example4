76cf0d7f204d841ebbcc96193b07705938eaf72b
tozkurt <ozkurtalip@gmail.com>
2020-11-24 20:33:32 +0100

    Sepete ekle kismi eklendi

--- src/Test.java
+++ src/Test.java
@@ -2,4 +2,10 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("1.islem");
     }
+
+    public static void yeniMethod()
+    {
+        System.out.println("yeni methodum hazir");
+        System.out.println("sepete ekle kisimlari buralar");
+    }
 }
